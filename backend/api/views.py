5d10cb5978aedff8200e9b59e52be148b376388a
AlexM2202 <alexmarqu22@gmail.com>
2025-07-02 16:55:07 -0500

    added /health endpoit for checking database status

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.db import connection, OperationalError
 
 # Create your views here.
+
+def health_check(request):
+    try:
+        connection.ensure_connection()
+        return JsonResponse({"status": "ok", "database": "connected"})
+    except OperationalError:
+        return JsonResponse({"status": "error", "database": "disconnected"})
